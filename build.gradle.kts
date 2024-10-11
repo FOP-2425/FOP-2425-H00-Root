7fe5bd9050e83540928c7978d3aff2aaacae5d5e
nyanyan <zentox@users.noreply.github.com>
2024-10-11 12:21:11 +0200

    Cleanup dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,45 @@
 plugins {
-    java
-    application
+ alias(libs.plugins.algomate)
+    alias(libs.plugins.jagr)
     alias(libs.plugins.style)
-    alias(libs.plugins.jagr.gradle)
 }
 
 version = file("version").readLines().first()
 
-jagr {
+exercise {
     assignmentId.set("h00")
-    submissions {
-        val main by creating {
-            studentId.set("ab12cdef")
-            firstName.set("sol_first")
-            lastName.set("sol_last")
+}
+
+submission {
+    // ACHTUNG!
+    // Setzen Sie im folgenden Bereich Ihre TU-ID (NICHT Ihre Matrikelnummer!), Ihren Nachnamen und Ihren Vornamen
+    // in Anführungszeichen (z.B. "ab12cdef" für Ihre TU-ID) ein!
+    studentId = "ab12cdef"
+    firstName = "sol_first"
+    lastName = "sol_last"
+
+    // Optionally require own tests for mainBuildSubmission task. Default is false
+    requireTests = false
+}
+
+dependencies {
+    implementation(libs.fopbot)
+}
+
+configurations.all {
+    resolutionStrategy {
+        configurations.all {
+            resolutionStrategy {
+                force(
+                    libs.algoutils.student,
+                    libs.algoutils.tutor,
+                )
+            }
         }
     }
+}
+
+jagr {
     graders {
         val graderPrivate by creating {
             graderName.set("H00-Private")
@@ -24,38 +48,5 @@ jagr {
                 implementation(libs.algoutils.tutor)
             }
         }
-    }
-}
-
-dependencies {
-    implementation(libs.annotations)
-    implementation(libs.algoutils.student)
-    implementation(libs.fopbot)
-    testImplementation(libs.junit.core)
-}
-
-application {
-    mainClass.set("h00.Main")
-}
-
-tasks {
-    val runDir = File("build/run")
-    withType<JavaExec> {
-        doFirst {
-            runDir.mkdirs()
-        }
-        workingDir = runDir
-    }
-    test {
-        doFirst {
-            runDir.mkdirs()
-        }
-        workingDir = runDir
-        useJUnitPlatform()
-    }
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        sourceCompatibility = "17"
-        targetCompatibility = "17"
     }
 }
